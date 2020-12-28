52e694294a9134f25d0659f8e7ec41ff6eee76cd
klaus_glb <klaus417_glb@hotmail.com>
2020-12-28 16:54:59 +0800

    feat: add icon.css

--- src/components/MiscRating.vue
+++ src/components/MiscRating.vue
@@ -1,55 +1,55 @@
 <template>
-    <v-card
-      :color="color"
-      class="mx-auto elevation-2"
-      dark
-    >
-        <!--          style="max-width: 400px;"-->
-        <v-row>
-            <v-col>
-                <v-card-title>
-                    <div style="color: #202124">
-                        <div class="headline">
-                            {{ name }}
+        <v-card
+          class="mx-auto elevation-2"
+          :color="color"
+          dark
+        >
+            <!--          style="max-width: 400px;"-->
+            <v-row>
+                <v-col>
+                    <v-card-title>
+                        <div style="color: #202124">
+                            <div class="headline">
+                                {{ name }}
+                            </div>
+                            <div>{{director}}</div>
+                            <div>({{year}})</div>
                         </div>
-                        <div>{{director}}</div>
-                        <div>({{year}})</div>
-                    </div>
-                </v-card-title>
-            </v-col>
-            <v-img
-              class="shrink ma-2"
-              contain
-              height="125px"
-              src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-              style="flex-basis: 125px"
-            ></v-img>
-        </v-row>
-        <v-divider dark></v-divider>
-        <v-card-actions class="pa-4" style="color: #202124">
-            给电影打分
-            <v-spacer></v-spacer>
-            <span class="black--text text--lighten-2 caption mr-2">
+                    </v-card-title>
+                </v-col>
+                <v-img
+                  class="shrink ma-2"
+                  contain
+                  height="125px"
+                  src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                  style="flex-basis: 125px"
+                ></v-img>
+            </v-row>
+            <v-divider dark></v-divider>
+            <v-card-actions class="pa-4" style="color: #202124">
+                给电影打分
+                <v-spacer></v-spacer>
+                <span class="black--text text--lighten-2 caption mr-2">
         ({{ rating }})
       </span>
-            <v-rating
-              background-color="#737373"
-              color="yellow accent-4"
-              dense
-              half-increments
-              hover
-              size="18"
-              v-model.number="rating"
-            ></v-rating>
-        </v-card-actions>
-    </v-card>
+                <v-rating
+                  background-color="#737373"
+                  color="yellow accent-4"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+                  v-model="rating"
+                ></v-rating>
+            </v-card-actions>
+        </v-card>
 </template>
 
 <script>
     export default {
         name: "MiscRating",
         props: {
-            rating: String,
+            rating: Number,
             name: String,
             director: String,
             year: String,
--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,25 +1,48 @@
 <template>
   <v-app id="BaseLayout">
     <v-app-bar
-      app
-      color="white"
+        app
+        color="white"
     >
       <!--            <v-toolbar-title class="align-self-center">电影推荐系统</v-toolbar-title>-->
 
       <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-        v-model="tab"
+          centered
+          class="ml-n9"
+          color="grey darken-1"
       >
         <v-tab
-          :key="i"
-          @click="$router.push(item.routerPath)"
-          v-for="(item, i) in items"
+            :key="i"
+            @click="$router.push(item.routerPath)"
+            v-for="(item, i) in items"
         >
-          {{item.name}}
+          {{ item.name }}
         </v-tab>
       </v-tabs>
+
+      <div v-on:keyup.enter="search">
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-text-field
+                class="mx-4"
+                flat
+                hide-details
+                label="Search"
+                append-icon="search"
+                solo-inverted
+                v-model="searchText"
+                ref="search"
+                v-on="on"
+            ></v-text-field>
+          </template>
+          <v-list v-if="choices.length > 0" class="border-list" dense>
+            <v-list-item v-for="(choice, index) in choices" :key="index" @click="itemClick(choice)">
+              <v-list-item-title>{{ choice.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
 
     </v-app-bar>
 
@@ -34,79 +57,122 @@
 </template>
 
 <script>
-  export default {
-    name: "BaseLayout",
-    data: () => ({
-      items: [
-        {
-          name: "近期热门",
-          routerPath: "/hot"
-        },
-        {
-          name: "排行榜",
-          routerPath: "/rank",
-          children: [
-            {
-              name: "test",
-              goto: '#test'
-            }
-          ]
-        },
-        {
-          name: "推荐电影",
-          routerPath: "/recommend"
-        },
+export default {
+  name: "BaseLayout",
+  data: () => ({
+    items: [
+      {
+        name: "近期热门",
+        routerPath: "/hot"
+      },
+      {
+        name: "排行榜",
+        routerPath: "/rank",
+        children: [
+          {
+            name: "test",
+            goto: '#test'
+          }
+        ]
+      },
+      {
+        name: "推荐电影",
+        routerPath: "/recommend"
+      }
+    ],
+    searchText: "",
+    choices: [],
+    showSelect: true
 
+
+  }),
+  watch: {
+    searchText: 'inputHandle'
+  },
+  methods: {
+    itemClick(choice) {
+      this.searchText = choice.name
+      this.$refs.search.blur()
+      // this.$router.push()
+    },
+    inputHandle(text) {
+      if (text.trim() !== '') {
+        this.showSelect = true
+        setTimeout(() => {
+          this.getEntity()
+        }, 300)
+      }
+    },
+    getEntity() {
+      // 请求接口更新choices数据
+      this.choices = [
+        {
+          key: '1234',
+          name: '1234'
+        },
+        {
+          key: 'abc',
+          name: 'abc'
+        }
       ]
-    })
+    },
+    search() {
+      this.$refs.search.blur()
+      console.log(this.searchText)
+      // this.$router.push()
+    }
   }
+}
 </script>
 
 <style scoped>
-  /* fade */
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.28s;
-  }
+/* fade */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.28s;
+}
 
-  .fade-enter,
-  .fade-leave-active {
-    opacity: 0;
-  }
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 
-  /* fade-transform */
-  .fade-transform-leave-active,
-  .fade-transform-enter-active {
-    transition: all .2s;
-  }
+/* fade-transform */
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all .2s;
+}
 
-  .fade-transform-enter {
-    opacity: 0;
-    transform: translateX(-30px);
-  }
+.fade-transform-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
 
-  .fade-transform-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
-  }
+.fade-transform-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 
-  /* breadcrumb transition */
-  .breadcrumb-enter-active,
-  .breadcrumb-leave-active {
-    transition: all .5s;
-  }
+/* breadcrumb transition */
+.breadcrumb-enter-active,
+.breadcrumb-leave-active {
+  transition: all .5s;
+}
 
-  .breadcrumb-enter,
-  .breadcrumb-leave-active {
-    opacity: 0;
-    transform: translateX(20px);
-  }
+.breadcrumb-enter,
+.breadcrumb-leave-active {
+  opacity: 0;
+  transform: translateX(20px);
+}
 
-  .breadcrumb-move {
-    transition: all .5s;
-  }
+.breadcrumb-move {
+  transition: all .5s;
+}
 
-  .breadcrumb-leave-active {
-    position: absolute;
-  }
+.breadcrumb-leave-active {
+  position: absolute;
+}
+.border-list {
+  border: 1px solid #eee !important;
+}
 </style>
